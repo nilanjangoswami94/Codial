6799ce48fda17f3f8b9cc3ff994d7a410c0fb7f6
nilanjangoswami94 <nilanjan.goswami94@gmail.com>
2022-01-21 23:38:58 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const homeController = require('../controllers/home_controller')
 
+const userController = require('../controllers/user_controller');
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+router.get('/profile', userController.profile)
+//for any further routes, access from here
+//router.use('./routerName', require('/routerfile));
 
 
 module.exports = router;
