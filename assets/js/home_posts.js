5ab80d0a14c3a52be7729f1043aa72be633694d6
nilanjangoswami94 <nilanjan.goswami94@gmail.com>
2022-03-03 00:50:58 +0530

    ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
                 url: '/posts/create',
                 data: newPostFrom.serialize(),
                 success: function(data){
-                    console.log(data);
+                    let newpost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
                 }, error: function(err){
                     console.log(error.responseText);
                 }
@@ -21,36 +22,31 @@
 
     //method to create a post in DOM
     let newPostDom = function(post){
-        return $(`<li id="post-<%= post._id %>">
+        return $(`<li id="post-${post._id}">
                     <p>
-                        <% if (locals.user && locals.user.id == post.user.id) { %>
                         <small>
-                            <a class="delete-post-button" href="/posts/destroy/<%= post.id %>">X</a>
+                            <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
                         </small>
                         <% } %>
-                        <%= post.content %>
+                        ${post.content}
                         <br>
                         <small>
-                            <%= post.user.name %>
+                        ${ post.user.name}
                         </small>
                     </p>
                     
                     <div class = "post-comments">
-                        <% if (locals.user) { %>
+                        
                                 <form action="/comments/create" method="POST">
                                     <input type="text" name="content" placeholder="Type here to add comment...">
-                                    <input type="hidden" name="post" value="<%= post._id %>">
+                                    <input type="hidden" name="post" value="${post._id}">
                                     <input type="submit" value="Add Comment">
                                 </form>
                         <% } %>
                 
                         <div class="post-comments-list">
-                            <ul id="post-comments-<%= post._id %>">
-                                <% for (comment of post.comments) { %>
-                
-                                    <%- include('_comment') -%>
-                                    
-                                <% } %>
+                            <ul id="post-comments-${post._id}">
+                                
                             </ul>
                         </div>
                     </div>
